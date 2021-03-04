ec082c1f819fd82c7ea2f8026189189466009b82
Tejas Anghan <tejanghan@gmail.com>
2021-03-04 09:31:17 +0530

    Added GetContractByEmail in DonationSyatem Contract

--- src/ethereum/contracts/Project.sol
+++ src/ethereum/contracts/Project.sol
@@ -181,15 +181,20 @@ contract DonationSystem{
     }
     
     function createUserContact(string memory userName , string memory userEmail , uint aadh , uint ph) public returns(address) {
+        require(getUserContract[userEmail] == address(0));
         address newUserContract = address (new User(msg.sender , adminContractAddress , approversContractAddress , hospitalsContractAddress , userName , userEmail, aadh , ph));
         delpoyedUsers.push(newUserContract);
-        // getUserContract[email] = newUserContract;
+        getUserContract[userEmail] = newUserContract;
         return address(newUserContract);
         
     }
     
     function getDeployedUsers() public view  returns(address[] memory){
         return delpoyedUsers;
+    }
+
+    function getUserContractAddress(string memory email) public view returns (address) {
+        return  getUserContract[email];
     }
     
 }
